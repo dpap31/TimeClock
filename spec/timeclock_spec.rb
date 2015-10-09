653b088439c402dccf2208271aa1d91bb84093d4
Drew Pappas <drewpappas@mac.com>
2015-10-08 18:58:47 -0700

    added timeclock specs

--- spec/timeclock_spec.rb
+++ spec/timeclock_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'initialize' do
+describe '#initialize' do
   context 'IN action' do
     before(:all) do
       params = { action: 'in', note: 'note' }
@@ -13,7 +13,7 @@ describe 'initialize' do
       expect(@tc.instance_variable_get(:@note)).to eq('note')
     end
     it 'has valid notification text' do
-      expect(@tc.instance_variable_get(:@notification)).to include('Clocked in at')
+      expect(@tc.instance_variable_get(:@notification)).to include('Clocked in')
     end
   end
   context 'OUT action' do
@@ -22,13 +22,45 @@ describe 'initialize' do
       @tc = TimeClock.new(params)
     end
     it 'has valid action' do
-      expect(@tc.instance_variable_get(:@out)).to eq(:out)
-    end
-    it 'has valid note' do
-      expect(@tc.instance_variable_get(:@note)).to eq('note')
+      expect(@tc.instance_variable_get(:@action)).to eq(:out)
     end
     it 'has valid notification text' do
-      expect(@tc.instance_variable_get(:@notification)).to include('Clocked out at')
+      expect(@tc.instance_variable_get(:@notification)).to include('Clocked out')
     end
+  end
+  context 'INVALID action' do
+    before(:all) do
+      params = { action: 'foo', note: 'note' }
+      @tc = TimeClock.new(params)
+    end
+    it 'has valid action' do
+      expect(@tc.instance_variable_get(:@action)).to eq(:invalid)
+    end
+  end
+end
+
+describe '#launch!' do
+  it 'IN display notification' do
+    tc = TimeClock.new(action: 'in')
+    expect(tc).to receive(:system).with(" echo \"Clocked in at #{Time.now.strftime('%I:%M %p')}\" ")
+    tc.launch!
+  end
+  it 'OUT display notification' do
+    tc = TimeClock.new(action: 'out')
+    expect(tc).to receive(:system).with(" echo \"Clocked out at #{Time.now.strftime('%I:%M %p')}\" ")
+    tc.launch!
+  end
+  it 'LAST display notification' do
+    clock_in = TimeClock.new(action: 'in')
+    expect(clock_in).to receive(:system).with(" echo \"Clocked in at #{Time.now.strftime('%I:%M %p')}\" ")
+    clock_in.launch!
+    tc = TimeClock.new(action: 'last')
+    expect(tc).to receive(:system).with(" echo \"Last punch was at: #{Time.now.strftime('%I:%M %p')}\" ")
+    tc.launch!
+  end
+  it 'OPEN display notification' do
+    punch = Punches.new()
+    expect(punch).to receive(:system).with(" open \"/Users/dpappas/Sites/timeclock/spec/punches.csv\" ")
+    punch.open_csv
   end
 end
